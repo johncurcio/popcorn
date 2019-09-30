b8281ff267bf8ccc9287e60d4aaf577c09dcbe70
John Curcio <john@curcio.dev>
2019-09-30 17:05:00 -0300

    Add hello world to bootsector boot

--- bootsector.asm
+++ bootsector.asm
@@ -2,9 +2,19 @@
 ; A disk is bootable if it occupies sections (cylinder, head, sector) = (0,0,0) 
 ; and it takes 512 bytes in disk and bytes 511 and 512 are 0xAA55
 
-loop:
-    jmp loop
+mov ah, 0x0e ;tty mode
+mov al, 'H'
+int 0x10 ;video interrupt
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+int 0x10
+mov al, 'o'
+int 0x10
 
-times 510-($-$$) db 0
+jmp $ ; infinite loop  
+
+times 510 - ($-$$) db 0
 
 dw 0xaa55
